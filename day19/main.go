6965a07166342f22306314d3d905ee6ddcaaf6ed
Ines Almeida <ines.almeida@canonical.com>
2024-12-22 23:20:06 +0100

    Day 19 - part 2

--- day19/main.go
+++ day19/main.go
@@ -52,7 +52,7 @@ func checkPatternPossible(pattern string) bool {
 	if ok {
 		// fmt.Println("Found in availale towels", pattern)
 		// fmt.Println("CACHE", cache)
-		cache[pattern] = true
+		cache[pattern] = 1
 		return true
 	}
 
@@ -60,20 +60,20 @@ func checkPatternPossible(pattern string) bool {
 	if ok {
 		// fmt.Println("Found in cache", pattern)
 		// fmt.Println("CACHE", cache)
-		return val
+		return val == 1
 	}
 
 	for i := range pattern {
 		_, ok := availableTowels[pattern[:i+1]]
 		if ok {
 			if checkPatternPossible(pattern[i+1:]) {
-				cache[pattern] = true
+				cache[pattern] = 1
 				// fmt.Println("Found that is possible ", pattern)
 				// fmt.Println("CACHE", cache)
 				return true
 			}
 		} else {
-			cache[pattern] = false
+			cache[pattern] = 0
 		}
 	}
 	return false
@@ -81,7 +81,7 @@ func checkPatternPossible(pattern string) bool {
 
 var availableTowels = make(map[string]bool)
 var patterns = make([]string, 0)
-var cache = make(map[string]bool)
+var cache = make(map[string]int)
 
 func parseInput(lines []string) {
 	towels := strings.Split(lines[0], ", ")
@@ -95,5 +95,47 @@ func parseInput(lines []string) {
 }
 
 func part2(lines []string) any {
-	return nil
+	cache = make(map[string]int)
+
+	result := 0
+	for _, pattern := range patterns {
+		result += countPatternPossibilities(pattern)
+	}
+
+	return result
+}
+
+func countPatternPossibilities(pattern string) int {
+
+	count := 0
+
+	_, ok := availableTowels[pattern]
+	if ok {
+		// fmt.Println("Found in available", pattern)
+		count += 1
+	}
+
+	val, ok := cache[pattern]
+	if ok {
+		// fmt.Println("Found in cache", pattern, val)
+		// fmt.Println("CACHE", cache)
+		return val
+	}
+
+	for i := range pattern {
+		if len(pattern) == len(pattern[:i+1]) {
+			// break when we are evaluating the exact pattern string
+			break
+		}
+
+		_, ok := availableTowels[pattern[:i+1]]
+		if ok {
+			possibilities := countPatternPossibilities(pattern[i+1:])
+			count += possibilities
+			// fmt.Println("Found that is possible ", pattern, pattern[i+1:], possibilities, count)
+		}
+	}
+
+	cache[pattern] = count
+	return count
 }
